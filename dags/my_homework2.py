f9bc47ae3d18525a9fd744b2d00668f51bd16ec9
NUTSRK <nutsrk@odds.team>
2021-09-27 22:47:44 +0700

    homework2

---
+++ dags/my_homework2.py
@@ -0,0 +1,48 @@
+import logging
+
+from airflow import DAG 
+from airflow.utils import timezone
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+
+def _say_hello():
+    logging.info("Hello, My Homework")
+
+def _print_log_messages():
+    logging.info("Sirirak, Nutsrk")
+
+default_args = {
+    "owner": "Nutsrk",
+    "start_date": timezone.datetime(2021, 9, 27)
+}
+with DAG(
+    "my_homework2",
+    schedule_interval="*/30 * * * *",
+    default_args=default_args,
+    catchup=False,
+    tags=["saksiam"],
+) as dag:
+
+
+    start = DummyOperator(task_id="start")
+
+    echo_hello = BashOperator(
+        task_id="echo_hello",
+        bash_command="echo 'hello'",
+    )
+
+    say_hello = PythonOperator(
+        task_id="say_hello",
+        python_callable=_say_hello,
+    )
+
+    print_log_messages = PythonOperator(
+        task_id="print_log_messages",
+        python_callable=_print_log_messages,
+    )
+
+    end = DummyOperator(task_id="end")
+  
+    start >> echo_hello >> say_hello >> print_log_messages >> end 
